7d68056bd913def4b3caf45b4dc8117a29406209
programmer <ildarinho99@gmail.com>
2023-07-28 02:05:26 +0500

    Написание sql запросов для создания таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    employee_id bigserial PRIMARY KEY NOT NULL,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    title text,
+    birth_date date,
+    notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id VARCHAR(50) PRIMARY KEY NOT NULL,
+    company_name VARCHAR(200),
+    contact_name VARCHAR(100)
+);
+
+
+CREATE TABLE orders
+(
+    order_id bigserial PRIMARY KEY NOT NULL,
+    customer_id VARCHAR(50) REFERENCES customers(customer_id),
+    employee_id bigserial REFERENCES employees(employee_id),
+    order_date date,
+    ship_city VARCHAR(100)
+);
+
